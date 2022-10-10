aaba7259fa3cb0fb949d8bbdd88b41a8c671f77d
Bruno Peres <bruno_16515@hotmail.com>
2022-10-10 14:23:45 -0300

    SumStrings adicionado

--- app.js
+++ app.js
@@ -376,3 +376,12 @@ function validISBN10(isbn) {
     return false;
   }
 }
+
+/* 
+Given the string representations of two integers, return the string representation of the sum of those integers.
+
+For example: sumStrings('1','2') // => '3' */
+
+function sumStrings(a, b) {
+  return (BigInt(a) + BigInt(b)).toString(10);
+}
