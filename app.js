087488ef8e094de00366a5d7d116970150c470fb
Bruno Peres <bruno_16515@hotmail.com>
2022-10-25 08:25:31 -0300

    :rocket: Adicionando exercícios

--- app.js
+++ app.js
@@ -619,9 +619,45 @@ Note: Math.pow and some other Math functions like eval() and ** are disabled.
 function numberToPower(number, power) {
   let x = number;
 
-  if (power === 0) return 1
+  if (power === 0) return 1;
 
-  for (let i = 1; i < power; i++) { x = x * number }
+  for (let i = 1; i < power; i++) {
+    x = x * number;
+  }
 
   return x;
 }
+
+/* You begin with writing a generic Ship class / struct:
+
+function Ship(draft,crew) {
+ this.draft = draft;
+ this.crew = crew;
+}
+Every time your spies see a new ship enter the dock, they will create a new ship object based on their observations:
+
+draft - an estimate of the ship's weight based on how low it is in the water
+crew - the count of crew on board
+var titanic = new Ship(15, 10);
+Task
+You have access to the ship "draft" and "crew". "Draft" is the total ship weight and "crew" is the number of humans on the ship.
+
+Each crew member adds 1.5 units to the ship draft. If after removing the weight of the crew, the draft is still more than 20, 
+then the ship is worth looting. Any ship weighing that much must have a lot of booty!
+
+Add the method
+
+isWorthIt
+to decide if the ship is worthy to loot. For example:
+
+titanic.isWorthIt() // return false */
+
+class Ship {
+  constructor(draft, crew) {
+    this.draft = draft;
+    this.crew = crew;
+  }
+  isWorthIt = () => this.draft - (this.crew * 1.5) > 20
+}
+
+
