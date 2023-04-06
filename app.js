78d5dafb4a6515f33b7713bbb08a924c1bffd6d3
Bruno Peres <bruno_16515@hotmail.com>
2023-04-06 16:56:25 -0300

    ✨ Exercício adicionado

--- app.js
+++ app.js
@@ -999,3 +999,20 @@ function isItANum(str) {
 
   return number.length === 11 && number[0] === "0" ? number : "Not a phone number";
 }
+
+
+/* Given an array of numbers, return the difference between the largest and smallest values.
+
+For example:
+
+[23, 3, 19, 21, 16] should return 20 (i.e., 23 - 3).
+
+[1, 434, 555, 34, 112] should return 554 (i.e., 555 - 1).
+
+The array will contain a minimum of two elements. Input data range guarantees that max-min will cause no integer overflow. */
+
+function betweenExtremes(numbers) {
+  const sorted = numbers.sort((a, b) => b - a);
+
+  return sorted[0] - sorted[sorted.length - 1];
+}
