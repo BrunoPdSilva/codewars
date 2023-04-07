e6bd32ed150e01877efe52830f748f6e38f52607
Bruno Peres <bruno_16515@hotmail.com>
2023-04-07 16:55:32 -0300

    ✨ Exercício adicionado

--- app.js
+++ app.js
@@ -1016,3 +1016,11 @@ function betweenExtremes(numbers) {
 
   return sorted[0] - sorted[sorted.length - 1];
 }
+
+/* 
+Task:
+Your job here is to write a function, which takes a sorted array ary and a value val, and returns the lowest index where you 
+could insert val to maintain the sorted-ness of the array. 
+The input array will always be sorted in ascending order. It may contain duplicates. */
+
+const keepOrder = (ary, val) => [...ary, val].sort((a, b) => a - b).indexOf(val);
